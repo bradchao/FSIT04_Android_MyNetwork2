41e97ba822ecff1d8812a06f3889644deed64fb8
Brad <brad@brad.tw>
2018-03-25 14:07:59 +0800

    v3

--- app/src/main/java/tw/org/iii/mynetwork2/MainActivity.java
+++ app/src/main/java/tw/org/iii/mynetwork2/MainActivity.java
@@ -1,16 +1,26 @@
 package tw.org.iii.mynetwork2;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -18,22 +28,56 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     private ImageView img;
     private UIHandler handler;
+    private File donloadPath;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    0);
+        }else{
+            init();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }else{
+            finish();
+        }
+    }
+
+    private void init(){
         img = findViewById(R.id.img);
         handler = new UIHandler();
+
+        donloadPath = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("Downloading...");
     }
+
 
     public void test1(View view) {
         new Thread(){
             @Override
             public void run() {
                 try {
-                    URL url = new URL("http://www.bradchao.com");
+                    URL url = new URL("http://10.0.3.2:8088/MyServer/brad01.jsp");
                     HttpURLConnection conn =
                             (HttpURLConnection)url.openConnection();
                     conn.connect();
@@ -68,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                     Bundle data = new Bundle();
                     data.putParcelable("bmp", bmp);
                     mesg.setData(data);
-
+                    mesg.what = 1;
                     handler.sendMessage(mesg);
                     //img.setImageBitmap(bmp);
 
@@ -86,13 +130,55 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
 
-            Bitmap bmp = (Bitmap)msg.getData().getParcelable("bmp");
-            img.setImageBitmap(bmp);
+            switch (msg.what){
+                case 1:
+                    Bitmap bmp = (Bitmap)msg.getData().getParcelable("bmp");
+                    img.setImageBitmap(bmp);
+                    break;
+                case 2:
+                    progressDialog.dismiss();
+                    break;
+            }
         }
     }
 
     public void test3(View view) {
+        progressDialog.show();
+        new Thread(){
+            @Override
+            public void run() {
+                getPDF();
+            }
+        }.start();
+
     }
+
+    private void getPDF(){
+        try{
+            URL url = new URL("http://pdfmyurl.com/?url=http://www.gamer.com.tw");
+            HttpURLConnection conn =
+                    (HttpURLConnection)url.openConnection();
+            conn.connect();
+
+            FileOutputStream outFile = new FileOutputStream(
+                    donloadPath.getAbsolutePath() + "/brad.pdf");
+            BufferedInputStream bin = new BufferedInputStream(conn.getInputStream());
+            byte[] buf = new byte[4096]; int len = 0;
+            while ( (len = bin.read(buf)) != -1){
+                outFile.write(buf,0, len);
+            }
+            outFile.flush();
+            outFile.close();
+            handler.sendEmptyMessage(2);
+        }catch (Exception e){
+            Log.v("brad", e.toString());
+        }
+
+
+
+    }
+
+
     public void test4(View view) {
     }
 }
